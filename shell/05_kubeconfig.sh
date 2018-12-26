d40fdabdd7df191706b08c42aee642d150b3a7b0
Chris Diehl <cultclassik@gmail.com>
2018-12-26 10:24:43 -0600

    Use ip for ssh connections

--- shell/05_kubeconfig.sh
+++ shell/05_kubeconfig.sh
@@ -112,12 +112,18 @@ done
 }
 
 for instance in kn1 kn2 kn3; do
+  THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
+  MY_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
+
   scp -i $SSHKEY \
-    ${instance}.kubeconfig kube-proxy.kubeconfig ${SSHUSR}@${instance}:~/
+    ${instance}.kubeconfig kube-proxy.kubeconfig ${SSHUSR}@${MY_IP}:~/
 done
 Copy the appropriate kube-controller-manager and kube-scheduler kubeconfig files to each controller instance:
 
 for instance in kc1 kc2 kc3; do
+  THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
+  MY_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
+
   scp -i $SSHKEY \
-    admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig ${SSHUSR}@${instance}:~/
+    admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig ${SSHUSR}@${MY_IP}:~/
 done
