3aaf7e353a70b975134f4bf662da69fab2e6c15d
Chris Diehl <cultclassik@gmail.com>
2018-12-26 12:29:31 -0600

    Fix typo

--- shell/07_etcd.sh
+++ shell/07_etcd.sh
@@ -46,7 +46,7 @@ ExecStart=/usr/local/bin/etcd \\
   --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
   --advertise-client-urls https://${INTERNAL_IP}:2379 \\
   --initial-cluster-token etcd-cluster-0 \\
-  --initial-cluster controller-0=https://${CLUSTER_NODE1}:2380,controller-1=https:/${CLUSTER_NODE2}:2380,controller-2=https://${CLUSTER_NODE3}:2380 \\
+  --initial-cluster controller-0=https://${CLUSTER_NODE1}:2380,controller-1=https://${CLUSTER_NODE2}:2380,controller-2=https://${CLUSTER_NODE3}:2380 \\
   --initial-cluster-state new \\
   --data-dir=/var/lib/etcd
 Restart=on-failure
