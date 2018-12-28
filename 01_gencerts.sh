9794f50a4a3f0a52b3b4c530a9e666e68cef9c96
Chris Diehl <cultclassik@gmail.com>
2018-12-28 07:18:43 -0600

    Update scripts

--- 01_gencerts.sh
+++ 01_gencerts.sh
@@ -5,9 +5,6 @@
 SSHUSR="chris"
 SSHKEY="/mnt/secure/keys/chris.key"
 
-this_box=`vboxmanage list vms | grep kws | awk '{ gsub("\"", ""); print $1 }'`
-KUBERNETES_PUBLIC_ADDRESS=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
-
 chmod -R +x ./shell/*
 
 ./shell/01_ssl/04_ca.sh
@@ -24,7 +21,14 @@ done
 ./shell/01_ssl/04.03_controller-manager.sh
 ./shell/01_ssl/04.04_kube-proxy.sh
 ./shell/01_ssl/04.05_scheduler-client.sh
-./shell/01_ssl/04.06_api-server.sh ${KUBERNETES_PUBLIC_ADDRESS}
+
+for instance in kc1 kc2 kc3 kws; do
+  THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
+  EXTERNAL_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
+  lb_address+="${EXTERNAL_IP},"
+done
+./shell/01_ssl/04.06_api-server.sh ${lb_address}
+
 ./shell/01_ssl/04.07_svcacct-keypair.sh
 ./shell/01_ssl/04.99_dist-keys.sh ${SSHUSR} ${SSHKEY}
 
--- shell/01_ssl/04.06_api-server.sh
+++ shell/01_ssl/04.06_api-server.sh
@@ -2,7 +2,7 @@
 
 # Run anywaywere, pass IP address of load balancer
 
-KUBERNETES_PUBLIC_ADDRESS=$1
+controllers=$1
 
 {
 
@@ -29,7 +29,7 @@ cfssl gencert \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
-  -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,kubernetes.default \
+  -hostname=${controllers}127.0.0.1,kubernetes.default \
   -profile=kubernetes \
   kubernetes-csr.json | cfssljson -bare kubernetes
 
