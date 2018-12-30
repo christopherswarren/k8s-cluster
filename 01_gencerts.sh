b2a6b601608b32ce5a6a0f2b6bd9d4bae7bf243e
Chris Diehl <cultclassik@gmail.com>
2018-12-30 08:54:53 -0600

    Add subdir for ssl when creating certs

--- 01_gencerts.sh
+++ 01_gencerts.sh
@@ -6,21 +6,22 @@ SSHUSR="chris"
 SSHKEY="/mnt/secure/keys/chris.key"
 
 chmod -R +x ./shell/*
+mkdir ./ssl && cd ./ssl
 
-./shell/01_ssl/04_ca.sh
-./shell/01_ssl/04.01_admin.sh
+../shell/01_ssl/04_ca.sh
+../shell/01_ssl/04.01_admin.sh
 
 for instance in kn1 kn2 kn3; do
   echo "Instance: ${instance}"
   this_box=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
   external_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
   internal_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-  ./shell/01_ssl/04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
+  ../shell/01_ssl/04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
 done
 
-./shell/01_ssl/04.03_controller-manager.sh
-./shell/01_ssl/04.04_kube-proxy.sh
-./shell/01_ssl/04.05_scheduler-client.sh
+../shell/01_ssl/04.03_controller-manager.sh
+../shell/01_ssl/04.04_kube-proxy.sh
+../shell/01_ssl/04.05_scheduler-client.sh
 
 lb_address=""
 for instance in kc1 kc2 kc3 kws; do
@@ -30,15 +31,15 @@ for instance in kc1 kc2 kc3 kws; do
   lb_address="${lb_address},${EXTERNAL_IP},${INTERNAL_IP}"
 done
 lb_address="${lb_address:1}"
-./shell/01_ssl/04.06_api-server.sh ${lb_address}
 
-./shell/01_ssl/04.07_svcacct-keypair.sh
-./shell/01_ssl/04.99_dist-keys.sh ${SSHUSR} ${SSHKEY}
+../shell/01_ssl/04.06_api-server.sh ${lb_address}
+../shell/01_ssl/04.07_svcacct-keypair.sh
+../shell/01_ssl/04.99_dist-keys.sh ${SSHUSR} ${SSHKEY}
 
 THIS_BOX=`vboxmanage list vms | grep kws | awk '{ gsub("\"", ""); print $1 }'`
 KUBERNETES_PUBLIC_ADDRESS=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
-./shell/05_kubeconfig.sh ${SSHUSR} ${SSHKEY} ${KUBERNETES_PUBLIC_ADDRESS}
-./shell/06_data-encryption-conf-key.sh ${SSHUSR} ${SSHKEY}
+../shell/05_kubeconfig.sh ${SSHUSR} ${SSHKEY} ${KUBERNETES_PUBLIC_ADDRESS}
+../shell/06_data-encryption-conf-key.sh ${SSHUSR} ${SSHKEY}
 
 # install etcd on controllers
 CLUSTER_BOX1=`vboxmanage list vms | grep kc1 | awk '{ gsub("\"", ""); print $1 }'`
--- shell/01_ssl/04.99_dist-keys.sh
+++ shell/01_ssl/04.99_dist-keys.sh
@@ -9,13 +9,18 @@ for instance in kn1 kn2 kn3; do
   THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
   MY_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
   scp -i ${SSHKEY} \
-    ca.pem ${instance}-key.pem ${instance}.pem ${SSHUSR}@${MY_IP}:~/
+    ../ssl/ca.pem ${instance}-key.pem ../ssl/${instance}.pem ../ssl/${SSHUSR}@${MY_IP}:~/
 done
 
 for instance in kc1 kc2 kc3; do
   THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
   MY_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
   scp -i ${SSHKEY} \
-    ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
-    service-account-key.pem service-account.pem ${SSHUSR}@${MY_IP}:~/
+    ../ssl/ca.pem \
+    ../ssl/ca-key.pem \
+    ../ssl/kubernetes-key.pem \
+    ../ssl/kubernetes.pem \
+    ../ssl/service-account-key.pem \
+    ../ssl/service-account.pem \
+    ${SSHUSR}@${MY_IP}:~/
 done
