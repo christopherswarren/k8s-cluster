caf0dde0f47a6b75be6a568b5eb89cabf932d837
Chris Diehl <cultclassik@gmail.com>
2018-12-30 09:20:10 -0600

    Fix paths

--- shell/04_gencerts.sh
+++ shell/04_gencerts.sh
@@ -5,20 +5,20 @@
 
 mkdir ../ssl && cd ../ssl
 
-./04_ssl/04_ca.sh
-./04_ssl/04.01_admin.sh
+./shell/04_ssl04_ca.sh
+./shell/04_ssl04.01_admin.sh
 
 for instance in kn1 kn2 kn3; do
   echo "Instance: ${instance}"
   this_box=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
   external_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
   internal_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-  ./04_ssl/04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
+  ./shell/04_ssl04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
 done
 
-./04_ssl/04.03_controller-manager.sh
-./04_ssl/04.04_kube-proxy.sh
-./04_ssl/04.05_scheduler-client.sh
+./shell/04_ssl04.03_controller-manager.sh
+./shell/04_ssl04.04_kube-proxy.sh
+./shell/04_ssl04.05_scheduler-client.sh
 
 lb_address=""
 for instance in kc1 kc2 kc3 kws; do
@@ -29,5 +29,5 @@ for instance in kc1 kc2 kc3 kws; do
 done
 lb_address=`echo ${lb_address} | cut -c 1-`
 
-./04_ssl/04.06_api-server.sh ${lb_address}
-./04_ssl/04.07_svcacct-keypair.sh
+./shell/04_ssl04.06_api-server.sh ${lb_address}
+./shell/04_ssl04.07_svcacct-keypair.sh
