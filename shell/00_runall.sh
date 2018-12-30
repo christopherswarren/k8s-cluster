7135ac9981d7cf2f15e5a39b525e61ad3a1060df
Chris Diehl <cultclassik@gmail.com>
2018-12-30 09:14:38 -0600

    refactor

---
+++ shell/00_runall.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SSHUSR="chris"
+SSHKEY="/mnt/secure/keys/chris.key"
+
+chmod -R +x ./*.sh
+
+./04_gencerts.sh
+./04_ssl/04.99_dist-keys.sh ${SSHUSR} ${SSHKEY}
+
+THIS_BOX=`vboxmanage list vms | grep kws | awk '{ gsub("\"", ""); print $1 }'`
+KUBERNETES_PUBLIC_ADDRESS=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
+./05_kubeconfig.sh ${SSHUSR} ${SSHKEY} ${KUBERNETES_PUBLIC_ADDRESS}
+./06_data-encryption-conf-key.sh ${SSHUSR} ${SSHKEY}
+
+# install etcd on controllers
+CLUSTER_BOX1=`vboxmanage list vms | grep kc1 | awk '{ gsub("\"", ""); print $1 }'`
+CLUSTER_NODE1=`vboxmanage guestproperty get ${CLUSTER_BOX1} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
+CLUSTER_BOX2=`vboxmanage list vms | grep kc2 | awk '{ gsub("\"", ""); print $1 }'`
+CLUSTER_NODE2=`vboxmanage guestproperty get ${CLUSTER_BOX2} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
+CLUSTER_BOX3=`vboxmanage list vms | grep kc3 | awk '{ gsub("\"", ""); print $1 }'`
+CLUSTER_NODE3=`vboxmanage guestproperty get ${CLUSTER_BOX3} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
+INITIAL_CLUSTER="kc1=https://${CLUSTER_NODE1}:2380,kc2=https://${CLUSTER_NODE2}:2380,kc3=https://${CLUSTER_NODE3}:2380"
+
+for instance in kc1 kc2 kc3; do
+  THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
+  EXTERNAL_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
+  INTERNAL_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
+
+  scp -i $SSHKEY ./shell/07_etcd.sh ${SSHUSR}@${EXTERNAL_IP}:/tmp
+  ssh -t -i $SSHKEY ${SSHUSR}@${EXTERNAL_IP} /tmp/07_etcd.sh $INTERNAL_IP $INITIAL_CLUSTER
+done
--- shell/04_gencerts.sh
+++ shell/04_gencerts.sh
@@ -2,26 +2,23 @@
 
 # Run from VBox host
 #KUBERNETES_PUBLIC_ADDRESS=$(hostname -I | awk '{print  $2}')
-SSHUSR="chris"
-SSHKEY="/mnt/secure/keys/chris.key"
 
-chmod -R +x ./shell/*
-mkdir ./ssl && cd ./ssl
+mkdir ../ssl && cd ../ssl
 
-./01_ssl/04_ca.sh
-./01_ssl/04.01_admin.sh
+./04_ssl/04_ca.sh
+./04_ssl/04.01_admin.sh
 
 for instance in kn1 kn2 kn3; do
   echo "Instance: ${instance}"
   this_box=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
   external_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
   internal_ip=`vboxmanage guestproperty get ${this_box} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-  ./01_ssl/04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
+  ./04_ssl/04.02_kubelet-client.sh ${instance} ${external_ip} ${internal_ip}
 done
 
-./01_ssl/04.03_controller-manager.sh
-./01_ssl/04.04_kube-proxy.sh
-./01_ssl/04.05_scheduler-client.sh
+./04_ssl/04.03_controller-manager.sh
+./04_ssl/04.04_kube-proxy.sh
+./04_ssl/04.05_scheduler-client.sh
 
 lb_address=""
 for instance in kc1 kc2 kc3 kws; do
@@ -32,29 +29,5 @@ for instance in kc1 kc2 kc3 kws; do
 done
 lb_address=`echo ${lb_address} | cut -c 1-`
 
-./01_ssl/04.06_api-server.sh ${lb_address}
-./01_ssl/04.07_svcacct-keypair.sh
-./01_ssl/04.99_dist-keys.sh ${SSHUSR} ${SSHKEY}
-
-THIS_BOX=`vboxmanage list vms | grep kws | awk '{ gsub("\"", ""); print $1 }'`
-KUBERNETES_PUBLIC_ADDRESS=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
-./05_kubeconfig.sh ${SSHUSR} ${SSHKEY} ${KUBERNETES_PUBLIC_ADDRESS}
-./06_data-encryption-conf-key.sh ${SSHUSR} ${SSHKEY}
-
-# install etcd on controllers
-CLUSTER_BOX1=`vboxmanage list vms | grep kc1 | awk '{ gsub("\"", ""); print $1 }'`
-CLUSTER_NODE1=`vboxmanage guestproperty get ${CLUSTER_BOX1} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-CLUSTER_BOX2=`vboxmanage list vms | grep kc2 | awk '{ gsub("\"", ""); print $1 }'`
-CLUSTER_NODE2=`vboxmanage guestproperty get ${CLUSTER_BOX2} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-CLUSTER_BOX3=`vboxmanage list vms | grep kc3 | awk '{ gsub("\"", ""); print $1 }'`
-CLUSTER_NODE3=`vboxmanage guestproperty get ${CLUSTER_BOX3} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-INITIAL_CLUSTER="kc1=https://${CLUSTER_NODE1}:2380,kc2=https://${CLUSTER_NODE2}:2380,kc3=https://${CLUSTER_NODE3}:2380"
-
-for instance in kc1 kc2 kc3; do
-  THIS_BOX=`vboxmanage list vms | grep ${instance} | awk '{ gsub("\"", ""); print $1 }'`
-  EXTERNAL_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/1/V4/IP" | awk '{ print $2}'`
-  INTERNAL_IP=`vboxmanage guestproperty get ${THIS_BOX} "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{ print $2}'`
-
-  scp -i $SSHKEY ./shell/07_etcd.sh ${SSHUSR}@${EXTERNAL_IP}:/tmp
-  ssh -t -i $SSHKEY ${SSHUSR}@${EXTERNAL_IP} /tmp/07_etcd.sh $INTERNAL_IP $INITIAL_CLUSTER
-done
+./04_ssl/04.06_api-server.sh ${lb_address}
+./04_ssl/04.07_svcacct-keypair.sh
